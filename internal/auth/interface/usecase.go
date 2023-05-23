bd26bf79e54922c868aca24a24ee25a731880281
SiropTime <sirop4beats@gmail.com>
2023-05-24 00:53:34 +0300

    [+] fix of hmac

--- internal/auth/interface/usecase.go
+++ internal/auth/interface/usecase.go
@@ -3,9 +3,6 @@ package authInterface
 import "gb-ui-core/internal/auth/model"
 
 type UseCase interface {
-	SignUp(params *model.SignUpRequest) (*model.SignUpResponse, error)
-	SignIn(params *model.SignInRequest) (*model.SignInResponse, error)
-	ValidateEmail(params *model.EmailValidateRequest) (*model.EmailValidateResponse, error)
 	ValidateService(params *model.AuthHeadersLogic) (bool, error)
 	GenerateAccessToken(refreshToken string, params *model.CreateAuthTokensLogic) (accessToken string, err error)
 	GenerateTokensPair(params *model.CreateAuthTokensLogic) (accessToken, refreshToken string, err error)
--- internal/auth/usecase/usecase.go
+++ internal/auth/usecase/usecase.go
@@ -1,12 +1,9 @@
 package usecase
 
 import (
-	"context"
-	"fmt"
 	"gb-ui-core/config"
 	authInterface "gb-ui-core/internal/auth/interface"
 	"gb-ui-core/internal/auth/model"
-	"gb-ui-core/internal/pkg/common"
 	"gb-ui-core/pkg/terrors"
 	"gb-ui-core/pkg/thttp/server"
 	tlogger "gb-ui-core/pkg/tlogger"
@@ -14,7 +11,6 @@ import (
 	"gb-ui-core/pkg/tutils/ptr"
 	"github.com/fiorix/go-redis/redis"
 	"github.com/sarulabs/di"
-	"math/rand"
 	"time"
 )
 
@@ -36,100 +32,8 @@ func BuildAuthUsecase(ctn di.Container) (interface{}, error) {
 	}, nil
 }
 
-func (as *AuthUC) SignUp(params *model.SignUpRequest) (*model.SignUpResponse, error) {
-	if params.BusinessData.INN == nil {
-		return nil, terrors.Raise(nil, 100013)
-	}
-	if params.AuthData.Password != params.AuthData.RepeatedPassword {
-		return nil, terrors.Raise(nil, 100014)
-	}
-	// TODO: add validation for inn by FNS api and getting company's name by same way
-	if params.BusinessData.Name == nil {
-		if len(*params.BusinessData.INN) == 12 {
-			params.BusinessData.Name = ptr.String(fmt.Sprintf("ИП %s", *params.PersonalData.FullName))
-		}
-
-	}
-
-	params.AuthData.Password = tsecure.CalcSignature(as.config.SecureConfig.Fernet.Key, params.AuthData.Password, tsecure.SHA512)
-
-	userId, err := as.authRepo.CreateUser(context.Background(), &model.CreateUserDAO{
-		FullName:         params.PersonalData.FullName,
-		Email:            &params.AuthData.Email,
-		Password:         &params.AuthData.Password,
-		Inn:              params.BusinessData.INN,
-		JobPosition:      params.PersonalData.Position,
-		City:             params.PersonalData.Geographic.City,
-		Country:          params.PersonalData.Geographic.Country,
-		BusinessName:     params.BusinessData.Name,
-		EconomicActivity: params.BusinessData.EconomicActivity,
-		Website:          params.BusinessData.Website,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	accessToken, refreshToken, err := as.GenerateTokensPair(&model.CreateAuthTokensLogic{
-		UserId: userId,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return &model.SignUpResponse{
-		AuthTokensLogic: model.AuthTokensLogic{
-			AccessToken:  accessToken,
-			RefreshToken: refreshToken,
-		},
-		Response: common.Response{
-			Message:      "Successfully created user",
-			InternalCode: 201,
-		},
-	}, nil
-}
-
-func (as *AuthUC) ValidateEmail(params *model.EmailValidateRequest) (*model.EmailValidateResponse, error) {
-	rand.Seed(time.Now().UnixNano())
-	valid := rand.Intn(2) == 1
-	return &model.EmailValidateResponse{
-		Response: common.Response{
-			Message:      "SUCCESS",
-			InternalCode: 200,
-		},
-		Valid: valid,
-	}, nil
-}
-
-func (as *AuthUC) SignIn(params *model.SignInRequest) (*model.SignInResponse, error) {
-	user, err := as.authRepo.FindUserByEmail(params.Email)
-	if err != nil {
-		return nil, err
-	}
-
-	inputPassword := tsecure.CalcSignature(as.config.SecureConfig.Fernet.Key, params.Password, tsecure.SHA512)
-
-	if user.Password != inputPassword {
-		return nil, terrors.Raise(nil, 100016)
-	}
-
-	accessToken, refreshToken, err := as.GenerateTokensPair(&model.CreateAuthTokensLogic{
-		UserId: user.UserId,
-	})
-
-	return &model.SignInResponse{
-		AuthTokensLogic: model.AuthTokensLogic{
-			AccessToken:  accessToken,
-			RefreshToken: refreshToken,
-		},
-		Response: common.Response{
-			Message:      "Successful login",
-			InternalCode: 200,
-		},
-	}, nil
-}
-
 func (as *AuthUC) ValidateService(params *model.AuthHeadersLogic) (bool, error) {
-	service, err := as.authRepo.FindServiceByName(params.PublicKey)
+	service, err := as.authRepo.FindServiceByPublicKey(params.PublicKey)
 	if err != nil {
 		return false, err
 	}
